b2ecfdf88770ddc6ccd9d52f9e456e346882de26
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-04 14:04:03 +0900

    fix setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only:[:show]
   def show
-    @user = User.find(current_user.id)
+    if current_user.id == params[:id].to_i
+      @user = User.find(params[:id])
+    else
+      redirect_to :root
+    end
   end
 end
