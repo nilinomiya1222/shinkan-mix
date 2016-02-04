53c365ed54666621ed25fb9b4daa10177415d80f
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-04 09:07:43 +0900

    fix setting

--- Capfile
+++ Capfile
@@ -24,4 +24,4 @@ require 'capistrano/rails/migrations'
 # require 'capistrano/passenger'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
-Dir.glob('lib/capistrano/tasks/*.rb').each { |r| import r }
+Dir.glob('config/lib/capistrano/tasks/*.rb').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,10 @@ set :rbenv_ruby, '2.1.7'
 set :log_level, :debug
 
 namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
   desc 'Create database'
   task :db_create do
@@ -69,71 +73,6 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
-  end
-end
-#unicornのpidファイル、設定ファイルのディレクトリを指定
-namespace :unicorn do
-  task :environment do
-    set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
-    set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
-  end
-
-#unicornをスタートさせるメソッド
-  def start_unicorn
-    within current_path do
-      execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
-    end
-  end
-
-#unicornを停止させるメソッド
-  def stop_unicorn
-    execute :kill, "-s QUIT $(< #{fetch(:unicorn_pid)})"
-  end
-
-#unicornを再起動するメソッド
-  def reload_unicorn
-    execute :kill, "-s USR2 $(< #{fetch(:unicorn_pid)})"
-  end
-
-#unicronを強制終了するメソッド
-  def force_stop_unicorn
-    execute :kill, "$(< #{fetch(:unicorn_pid)})"
-  end
-
-#unicornをスタートさせるtask
-  desc "Start unicorn server"
-  task start: :environment do
-    on roles(:app) do
-      start_unicorn
-    end
-  end
-
-#unicornを停止させるtask
-  desc "Stop unicorn server gracefully"
-  task stop: :environment do
-    on roles(:app) do
-      stop_unicorn
-    end
-  end
-
-#既にunicornが起動している場合再起動を、まだの場合起動を行うtask
-  desc "Restart unicorn server gracefully"
-  task restart: :environment do
-    on roles(:app) do
-      if test("[ -f #{fetch(:unicorn_pid)} ]")
-        reload_unicorn
-      else
-        start_unicorn
-      end
-    end
-  end
-
-#unicornを強制終了させるtask 
-  desc "Stop unicorn server immediately"
-  task force_stop: :environment do
-    on roles(:app) do
-      force_stop_unicorn
     end
   end
 end
