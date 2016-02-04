abd6812c6bfcd6f6812e248d66a8e659d5fdacf9
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-04 16:37:14 +0900

    fix after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :user_registered_express?, if: :devise_controller?
   before_action :set_status, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -11,34 +12,22 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    unless admin_user_signed_in?
-      if current_user.owner?
-        if user_registered_express?(current_user)
-          new_circle_path
-          return
-        elsif user_registered_circle?(current_user)
-          circle = Circle.where(email: current_user.email)
-          circle.update(circle.length, user_id: current_user.id, status: 1)
-          redirect_to user_path(current_user) and return
-        else
-          root_path
-          return
-        end
-      else
-        root_path
-        return
-      end
-    else
+    if admin_user_signed_in?
       rails_admin_path
     end
   end
 
-  def user_registered_express?(user)
-    Circle.where(email: user.email).blank?
+  def register_express_circle
+    circle = Circle.where(email: current_user.email)
+    circle.update(circle.length, user_id: current_user.id, status: 1)
   end
 
-  def user_registered_circle?(user)
-    user.circles.blank?
+  def user_registered_express?
+    if user_signed_in?
+      if current_user.circles.blank? && Circle.where(email: current_user.email).present?
+        register_express_circle
+      end
+    end
   end
 
   def set_status
