5b1039220d9f5e5f33b750a5273c2cac87f8c52b
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-06 19:09:29 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       post 'search'
     end
   end
+  resources :likes, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
