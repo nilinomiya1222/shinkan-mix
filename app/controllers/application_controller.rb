4fa36ad4cf14e1a7572f27372c9a9628858bb66e
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-06 12:45:47 +0900

    delete needless action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :user_registered_express?, if: :devise_controller?
   before_action :set_status, if: :devise_controller?
 
   def configure_permitted_parameters
