0c933ad05d5294d6e07c45536729d1669eb9e208
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-02-06 19:08:29 +0900

    define action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,10 @@
 class LikesController < ApplicationController
+  def create
+    @circle = Circle.find(params[:circle_id])
+    current_user.likes.create(circle_id: params[:circle_id])
+  end
+  def destroy
+    Like.find_by(user_id: current_user.id, circle_id: params[:id]).destroy
+    @circle = Circle.find(params[:circle_id])
+  end
 end
