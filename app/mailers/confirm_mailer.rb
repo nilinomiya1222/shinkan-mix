4fb3ac134330a0719e05eb33fd907357fe7cb2c1
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-03-28 00:39:23 +0900

    remove circle unregister events

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -9,14 +9,16 @@ class ConfirmMailer < ApplicationMailer
     if @circle = Circle.find_by(id: c_id)
       @event_term = EventTerm.find_by(status: 1)
       @events = @circle.events.where(event_term_id: @event_term.id)
-      @st_date = @event_term.start_date.strftime('%m月%d日')
-      @fn_date = @event_term.finish_date.strftime('%m月%d日')
-      mail(
-      to:      "#{@circle.email}",
-      subject: "【要確認】新歓MiXイベント情報について",
-      from: "新歓MiX編集部",
-      ) do |format|
-        format.text
+      unless @events.blank?
+        @st_date = @event_term.start_date.strftime('%m月%d日')
+        @fn_date = @event_term.finish_date.strftime('%m月%d日')
+        mail(
+        to:      "#{@circle.email}",
+        subject: "【要確認】新歓MiXイベント情報について",
+        from: "新歓MiX編集部",
+        ) do |format|
+          format.text
+        end
       end
     end
   end
