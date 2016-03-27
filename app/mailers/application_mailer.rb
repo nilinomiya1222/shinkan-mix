335687283d941634c69cd7126dbc43624975b0ea
Tomoki Ninomiya <nilinomiya@gmail.com>
2016-03-28 00:17:33 +0900

    generate mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "新歓MiX編集部", charset: 'UTF-8'
+  layout 'mailer'
+end
---
+++ app/mailers/confirm_mailer.rb
@@ -0,0 +1,10 @@
+class ConfirmMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.confirm_mailer.sendmail_event.subject
+  #
+  def sendmail_event
+  end
+end
